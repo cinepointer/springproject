45a4cbffef754fd427d9cca0ae82a9a0ecf18576
jjs <wjdwhdtjr416@naver.com>
2025-05-14 09:48:17 +0900

    ㅁ

--- src/main/java/com/cinepointer/dao/userDao.java
+++ src/main/java/com/cinepointer/dao/userDao.java
@@ -1,22 +1,22 @@
 package com.cinepointer.dao;
 
-import com.cinepointer.dto.usersDto;
+import com.cinepointer.dto.UsersDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
-public interface userDao {
-    usersDto selectUserById(@Param("user_id") String user_id);
-    void insertUser(usersDto user);
+public interface UserDao {
+    UsersDto selectUserById(@Param("userId") String userId);
+    void insertUser(UsersDto user);
 
     // 회원정보 수정
-    void updateUser(usersDto user);
+    void updateUser(UsersDto user);
 
     // 회원탈퇴(삭제)
-    void deleteUser(@Param("user_id") String user_id);
+    void deleteUser(@Param("userId") String userId);
 
     // 전체 회원 리스트 조회 (권한 관리용)
-    List<usersDto> selectAllUsers();
+    List<UsersDto> selectAllUsers();
 }
--- src/main/java/com/cinepointer/dto/usersDto.java
+++ src/main/java/com/cinepointer/dto/usersDto.java
@@ -1,48 +1,48 @@
 package com.cinepointer.dto;
 
-public class usersDto {
-    private String user_id;
-    private String user_passwd;
-    private String user_name;
-    private String user_email;
-    private String user_status;
-    private String user_land_cd;
-    private String user_birth_date;
+public class UsersDto {
+    private String userId;
+    private String userPasswd;
+    private String userName;
+    private String userEmail;
+    private String userStatus;
+    private String userLandCd;
+    private String userBirthDate;
 
     // 기본 생성자
-    public usersDto() {}
+    public UsersDto() {}
 
     // 전체 필드 생성자
-    public usersDto(String user_id, String user_passwd, String user_name, String user_email,
-                    String user_status, String user_land_cd, String user_birth_date) {
-        this.user_id = user_id;
-        this.user_passwd = user_passwd;
-        this.user_name = user_name;
-        this.user_email = user_email;
-        this.user_status = user_status;
-        this.user_land_cd = user_land_cd;
-        this.user_birth_date = user_birth_date;
+    public UsersDto(String userId, String userPasswd, String userName, String userEmail,
+                    String userStatus, String userLandCd, String userBirthDate) {
+        this.userId = userId;
+        this.userPasswd = userPasswd;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userStatus = userStatus;
+        this.userLandCd = userLandCd;
+        this.userBirthDate = userBirthDate;
     }
 
     // Getter & Setter
-    public String getUser_id() { return user_id; }
-    public void setUser_id(String user_id) { this.user_id = user_id; }
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
 
-    public String getUser_passwd() { return user_passwd; }
-    public void setUser_passwd(String user_passwd) { this.user_passwd = user_passwd; }
+    public String getUserPasswd() { return userPasswd; }
+    public void setUserPasswd(String userPasswd) { this.userPasswd = userPasswd; }
 
-    public String getUser_name() { return user_name; }
-    public void setUser_name(String user_name) { this.user_name = user_name; }
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
 
-    public String getUser_email() { return user_email; }
-    public void setUser_email(String user_email) { this.user_email = user_email; }
+    public String getUserEmail() { return userEmail; }
+    public void setUserEmail(String userEmail) { this.userEmail = userEmail; }
 
-    public String getUser_status() { return user_status; }
-    public void setUser_status(String user_status) { this.user_status = user_status; }
+    public String getUserStatus() { return userStatus; }
+    public void setUserStatus(String userStatus) { this.userStatus = userStatus; }
 
-    public String getUser_land_cd() { return user_land_cd; }
-    public void setUser_land_cd(String user_land_cd) { this.user_land_cd = user_land_cd; }
+    public String getUserLandCd() { return userLandCd; }
+    public void setUserLandCd(String userLandCd) { this.userLandCd = userLandCd; }
 
-    public String getUser_birth_date() { return user_birth_date; }
-    public void setUser_birth_date(String user_birth_date) { this.user_birth_date = user_birth_date; }
+    public String getUserBirthDate() { return userBirthDate; }
+    public void setUserBirthDate(String userBirthDate) { this.userBirthDate = userBirthDate; }
 }
