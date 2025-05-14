0638a1103fccab27ea7d0f4ddafe58908b900182
ibo12224 <ibo12224>
2025-05-14 09:50:53 +0900

    merge

--- src/main/java/com/cinepointer/dto/usersDto.java
+++ src/main/java/com/cinepointer/dto/usersDto.java
@@ -1,6 +1,5 @@
 package com.cinepointer.dto;
 
-<<<<<<< HEAD
 import lombok.Data;
 
 @Data
@@ -12,51 +11,5 @@ public class usersDto {
     private String userStatus;
     private String userLandCd;
     private String userBirthDate;
-=======
-public class UsersDto {
-    private String userId;
-    private String userPasswd;
-    private String userName;
-    private String userEmail;
-    private String userStatus;
-    private String userLandCd;
-    private String userBirthDate;
 
-    // 기본 생성자
-    public UsersDto() {}
-
-    // 전체 필드 생성자
-    public UsersDto(String userId, String userPasswd, String userName, String userEmail,
-                    String userStatus, String userLandCd, String userBirthDate) {
-        this.userId = userId;
-        this.userPasswd = userPasswd;
-        this.userName = userName;
-        this.userEmail = userEmail;
-        this.userStatus = userStatus;
-        this.userLandCd = userLandCd;
-        this.userBirthDate = userBirthDate;
-    }
-
-    // Getter & Setter
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
-
-    public String getUserPasswd() { return userPasswd; }
-    public void setUserPasswd(String userPasswd) { this.userPasswd = userPasswd; }
-
-    public String getUserName() { return userName; }
-    public void setUserName(String userName) { this.userName = userName; }
-
-    public String getUserEmail() { return userEmail; }
-    public void setUserEmail(String userEmail) { this.userEmail = userEmail; }
-
-    public String getUserStatus() { return userStatus; }
-    public void setUserStatus(String userStatus) { this.userStatus = userStatus; }
-
-    public String getUserLandCd() { return userLandCd; }
-    public void setUserLandCd(String userLandCd) { this.userLandCd = userLandCd; }
-
-    public String getUserBirthDate() { return userBirthDate; }
-    public void setUserBirthDate(String userBirthDate) { this.userBirthDate = userBirthDate; }
->>>>>>> branch 'master' of https://github.com/cinepointer/springproject.git
 }
