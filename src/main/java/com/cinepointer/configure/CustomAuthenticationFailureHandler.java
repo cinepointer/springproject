ae0c3bddcddce26b969fd7478372a1abb103613c
ibo12224 <ibo12224>
2025-05-16 09:39:51 +0900

    로그인 오류처리 기능 수정, 이제 정상적으로 오류가 출력됩니다.

--- src/main/java/com/cinepointer/configure/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/cinepointer/configure/CustomAuthenticationFailureHandler.java
@@ -16,26 +16,22 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        String errormsg = "아이디나 비밀번호가 맞지 않습니다.";
 
-	@Override
-	public void onAuthenticationFailure(HttpServletRequest request,
-										HttpServletResponse response,
-										AuthenticationException exception) throws IOException, ServletException {
-		
-		String id = request.getParameter("id");
-		String errormsg = "";
-		
-		if(exception instanceof BadCredentialsException) {
-			errormsg = "아이디나 비밀번호가 맞지 않습니다. 다시 확인해 주세요";
-		} else if(exception instanceof InternalAuthenticationServiceException) {
-			errormsg = "아이디나 비밀번호가 맞지 않습니다. 다시 확인해 주세요";
-		} else if(exception instanceof DisabledException) {
-			errormsg = "계정이 비활성화되었습니다. 관리자에게 문의하세요.";
-		} else if(exception instanceof CredentialsExpiredException) {
-			errormsg = "비밀번호 유효기간이 만료되었습니다. 관리자에게 문의하세요.";
-		}
-		
-        response.sendRedirect("/signin?error=true");
+        if (exception instanceof DisabledException) {
+            errormsg = "계정이 비활성화되었습니다. 관리자에게 문의하세요.";
+        } else if (exception instanceof CredentialsExpiredException) {
+            errormsg = "비밀번호 유효기간이 만료되었습니다. 관리자에게 문의하세요.";
+        }
+        System.out.println("\n\n로그인 실패 원인: " + exception.toString());
+        // 에러 메시지를 세션에 저장
+        request.getSession().setAttribute("errorMessage", errormsg);
+
+        // 로그인 페이지로 리다이렉트
+        response.sendRedirect("/signin");
     }
 }
-
